c38e93f5f7c1c9952da572b612d3769332f6a858
Maxwell Asante <zhino4u2010@gmail.com>
2023-02-27 02:14:55 +0000

    initial commit

--- exceptions.py
+++
@@ -1,7 +0,0 @@
-try:
-    int("a")
-except ValueError as e:
-    print("oops u cant do that",e)
-    
-    
-print("this is the end of my program")
--- project.py
+++
@@ -1,9 +0,0 @@
-def upper_case(name):
-    return name.upper()
-
-if __name__ == "__main__":
-    name = "Maxwell"
-    name_upper = upper_case(name)
-    print(f"Upper case name is {name_upper}")
-
-    print("dunder name", __name__)
